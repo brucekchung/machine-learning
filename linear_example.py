e7a4cea53db9db8fc161b9da4e7c92b5109a18ee
Bruce Chung <brucekchung@gmail.com>
2018-07-09 11:23:38 -0600

    Add example 1-1

---
+++ linear_example.py
@@ -0,0 +1,30 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn
+
+#Load the data
+oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t',
+        encoding='latin1', na_values="n/a")
+
+#Prepare the data
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+X = np.c_[country_stats["GDP per capita"]]
+y = np.c_[country_stats["life satisfaction"]]
+
+#Visualize the data
+country_stats.plot(kind='scatter', x="GDP per capita", y='Life satisfaction')
+plt.show()
+
+#Select a linear model
+model = sklearn.linear_model.LinearRegression()
+
+# Train the model
+model.fit(X, y)
+
+#Make a prediction for Cyprus
+X_new = [[22587]]  #Cyrpus' GDP per capita
+print(mode.predict(X_new))
+
